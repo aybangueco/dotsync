00236bcc49b8064ea71aa88c40f3c3ef552d0466
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-08-02 22:29:36 +0800

    feat: add version command

---
+++ cmd/version.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/urfave/cli/v3"
+)
+
+var VersionCommand = &cli.Command{
+	Name:  "version",
+	Usage: "Display's current version number",
+	Action: func(ctx context.Context, c *cli.Command) error {
+		fmt.Println("DotSync v1.0.0")
+		return nil
+	},
+}

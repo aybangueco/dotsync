60851c473ebdb7a6d9f318436390843763bd4060
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-08-02 22:30:09 +0800

    Official Release v1.0.0

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/aybangueco/dotsync/cmd"
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "dotsync",
+		Usage: "Easily sync your dotfiles changes from your local repository to a specified path",
+		Commands: []*cli.Command{
+			cmd.InitCommand,
+			cmd.SyncCommand,
+			cmd.VersionCommand,
+		},
+	}
+
+	err := cmd.Run(context.Background(), os.Args)
+	if err != nil {
+		log.Println(err)
+	}
+}
